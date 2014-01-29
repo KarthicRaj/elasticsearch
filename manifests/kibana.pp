0825c4607fa00bc3bde5e88a89509098a7e272da
kenny13 <kenny13@gmail.com>
2014-01-29 23:59:36 +0200

    fix kibana install permissions on prod

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -8,11 +8,15 @@ apache::vhost { "kibana.local.host":
     docroot => "/var/www/kibana",
 }
 
+#chcon -R -v -t httpd_sys_rw_content_t kibana/
 #
 # Install kibana
 #
 $enhancers = [ "tar", "wget" ]
 package { $enhancers: ensure => "installed" }
+
+Exec { user => "apache" }
+File { owner => "apache", group => "apache" }
 
 exec { "getKibana" :
 	command => "/usr/bin/wget https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0milestone4.tar.gz -O /tmp/kibana-3.tar.gz",
@@ -31,6 +35,7 @@ exec {"unzipKibana":
 
 file { "/var/www/kibana":
 	ensure  => directory,
+    seltype => "httpd_sys_content_t",
     require => [ Class ["apache"] ]   
 }
 
@@ -40,7 +45,14 @@ exec {"cleanKibanaWwwDir":
 
 exec {"moveKibanaToWwwDir":
     command => "/bin/mv -f /tmp/kibanaUnziped/kibana-3.0.0milestone4/** /var/www/kibana",
-    require => [ File["/var/www/kibana"], Exec["unzipKibana"], Exec["cleanKibanaWwwDir"] ],
+    require => [ File["/var/www/kibana"], Exec["unzipKibana"], Exec["cleanKibanaWwwDir"], User["apache"] ]
 }
+
+exec {"chconKibana":
+    command => "/usr/bin/chcon -R -v -t httpd_sys_rw_content_t /var/www/kibana",
+    require => Exec["moveKibanaToWwwDir"]
+}
+
+
 
 info "http://localhost:8080/"
